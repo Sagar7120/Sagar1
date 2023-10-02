26fe425a7d9dd547adc3f1e0fd633644e05bc280
Sagar Shelki <sagarshelki30@gmail.com>
2023-10-02 10:52:14 +0000

    JavascriptBasics

---
+++ basic.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+
+const accountId = 121212
+let accountPassword = "232323"
+var accountEmail = "Sagar@gmail.com"
+
+
+//console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword])
