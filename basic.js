c4b315495cafe335f765f3e23b55882cec5227fb
Sagar Shelki <sagarshelki30@gmail.com>
2023-10-04 15:53:12 +0000

    minor changes

--- basic.js
+++ basic.js
@@ -1,9 +1,26 @@
-console.log("Hello")
+// console.log("Hello")
 
-const accountId = 121212
-let accountPassword = "232323"
-var accountEmail = "Sagar@gmail.com"
+// const accountId = "sagar1"
+// let accountPassword = Number(accountId)
+// var accountEmail = "Sagar@gmail.com"
+// let para = "the type of account id is "
 
 
 //console.log(accountPassword);
-console.table([accountEmail,accountId,accountPassword])
+//console.table([accountEmail,accountId,accountPassword])
+//console.log(typeof accountPassword)
+
+// console.log(para + accountId);
+
+// let son = ""
+// let account = Boolean(son)
+
+// console.log(account);
+// console.log(account);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
