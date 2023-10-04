c6e9710327b78ef3c995c2ac6c06547b50c0ef4f
Sagar Shelki <sagarshelki30@gmail.com>
2023-10-04 17:02:06 +0000

    basics

---
+++ comparators.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2 != 1);
+
+console.log("2" === 2);
+console.log(48 % 7);
+
+// Non premitive data types
+// Array, Object, Function
+
+let users = ["Sagar", "Nitin", "Aashish"];
+
+let myObj = {
+    name : "sagar",
+    age : 23
+}
+
+const myFunc = function (){
+    console.log("Hello World");
+    
+}
+
